591f4e6fcec46924af7bea10e5fe3778e34fde4d
nikolabibercic <nikolabibercic@gmail.com>
2020-08-15 21:46:25 +0200

    Ispravio sam link kod kategorije, kada se klikne na kategoriju oglasa pa na link za pretragu vodio je na pogresnu stranu

--- category.adds.view.php
+++ category.adds.view.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-3"></div>
         <div class="col-6">
-            <br><h2>Nazad na <a href="index.php">pretragu</a></h2>
+            <br><h2>Nazad na <a href="search.view.php">pretragu</a></h2>
         </div>
         <div class="col-3"></div>
     </div>
